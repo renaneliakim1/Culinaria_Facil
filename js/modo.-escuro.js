ccb01659faaf2e8cf067d025df7cd5e435f90beb
renaneliakim1 <renaneliakim1@gmail.com>
2024-04-22 11:46:10 -0300

    js: theme always fixed according to user, and border in #search

--- js/modo.-escuro.js
+++ js/modo.-escuro.js
@@ -1,4 +1,4 @@
-
+/* 
 document.getElementById("flexSwitchCheckDefault").addEventListener("click", function () {
     document.body.classList.toggle("modo-escuro");
     // Adiciona ou remove a classe 'svg-branco' dependendo se o modo escuro está ativado ou não
@@ -7,13 +7,10 @@ document.getElementById("flexSwitchCheckDefault").addEventListener("click", func
         svg.classList.toggle("svg-branco", document.body.classList.contains("modo-escuro"));
     });
 });
+ */
 
-
-
-window.onscroll = function () { scrollFunction() };
-
-
-
+ window.onscroll = function () { scrollFunction() };
+ 
 /*   // modo escuro
        
        document
@@ -21,3 +18,68 @@ window.onscroll = function () { scrollFunction() };
             .addEventListener("click", function () {
                 document.body.classList.toggle("modo-escuro");
             }); */
+
+
+
+
+/* ----------------------------------------------------------------------------------- */
+
+// Função para definir o modo escuro
+function enableDarkMode() {
+    document.body.classList.add('modo-escuro');
+    localStorage.setItem('modo', 'escuro');
+}
+
+// Função para definir o modo claro
+function enableLightMode() {
+    document.body.classList.remove('modo-escuro');
+    localStorage.setItem('modo', 'claro');
+}
+
+// Verificar a preferência do usuário ao carregar a página
+window.onload = function() {
+    const modoSalvo = localStorage.getItem('modo');
+
+    if (modoSalvo === 'escuro') {
+        enableDarkMode();
+    } else {
+        enableLightMode();
+    }
+};
+
+// Adicionar um evento de clique para alternar entre os modos
+document.getElementById('flexSwitchCheckDefault').addEventListener('click', function() {
+    if (document.body.classList.contains('modo-escuro')) {
+        enableLightMode();
+    } else {
+        enableDarkMode();
+    }
+});
+
+
+// Função para definir o modo escuro
+function enableDarkMode() {
+    document.body.classList.add('modo-escuro');
+    localStorage.setItem('modo', 'escuro');
+    // Altera a cor dos SVGs para branco
+    const svgs = document.querySelectorAll("svg");
+    svgs.forEach(svg => {
+        svg.classList.add("svg-branco");
+    });
+}
+
+// Função para definir o modo claro
+function enableLightMode() {
+    document.body.classList.remove('modo-escuro');
+    localStorage.setItem('modo', 'claro');
+    // Altera a cor dos SVGs de volta para a cor original
+    const svgs = document.querySelectorAll("svg");
+    svgs.forEach(svg => {
+        svg.classList.remove("svg-branco");
+    });
+}
+
+
+
+
+

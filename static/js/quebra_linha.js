70fa2e84e78e7cb0d4c8b1fa207d5ec567e2e6f5
PedroLucas33 <bobinav13@gmail.com>
2024-05-08 17:30:46 -0300

    fix texto receita

--- static/js/quebra_linha.js
+++ static/js/quebra_linha.js
@@ -4,8 +4,10 @@ var ingredientesElement = document.querySelector('.ingredientes li');
 // Obtém o texto dentro da <li>
 var texto = ingredientesElement.textContent;
 
-// Divide o texto em linhas
-var linhas = texto.split('\n');
+// Remove as linhas vazias e depois divide o texto em linhas
+var linhas = texto.split('\n').filter(function(linha) {
+    return linha.trim() !== ''; // Remove as linhas que contêm apenas espaços em branco
+});
 
 // Limpa o conteúdo da <li>
 ingredientesElement.textContent = '';
@@ -13,13 +15,12 @@ ingredientesElement.textContent = '';
 // Seleciona o elemento pai da <li>
 var listaElement = ingredientesElement.parentNode;
 
-// Para cada linha, cria um novo item de lista e adiciona à lista
+// Para cada linha não vazia, cria um novo item de lista e adiciona à lista
 linhas.forEach(function(linha) {
     var itemLista = document.createElement('li');
     itemLista.textContent = linha;
     listaElement.appendChild(itemLista);
 });
-
 
 // Seleciona o elemento que contém o texto
 var preparoElement = document.querySelector('.preparo li');
@@ -27,8 +28,10 @@ var preparoElement = document.querySelector('.preparo li');
 // Obtém o texto dentro da <li>
 var texto = preparoElement.textContent;
 
-// Divide o texto em linhas
-var linhas = texto.split('\n');
+// Remove as linhas vazias e depois divide o texto em linhas
+var linhas = texto.split('\n').filter(function(linha) {
+    return linha.trim() !== ''; // Remove as linhas que contêm apenas espaços em branco
+});
 
 // Limpa o conteúdo da <li>
 preparoElement.textContent = '';
@@ -36,7 +39,7 @@ preparoElement.textContent = '';
 // Seleciona o elemento pai da <li>
 var listaElement = preparoElement.parentNode;
 
-// Para cada linha, cria um novo item de lista e adiciona à lista
+// Para cada linha não vazia, cria um novo item de lista e adiciona à lista
 linhas.forEach(function(linha) {
     var itemLista = document.createElement('li');
     itemLista.textContent = linha;

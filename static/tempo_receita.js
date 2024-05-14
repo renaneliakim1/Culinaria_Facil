7e6aa3952a315f5fc6bf458650b14f51e5d74c38
dev_test <anny.mello1617@hotmail.com>
2024-05-14 15:51:46 -0300

    deletion of img, db and unnecessary comments

--- static/tempo_receita.js
+++
@@ -1,4 +0,0 @@
-function outputUpdate(tempo_preparo) {
-    var tempo_inteiro = Math.round(tempo_preparo);
-    document.querySelector('#tempo_selecionado').textContent = tempo_inteiro + ' minutos';
-}
